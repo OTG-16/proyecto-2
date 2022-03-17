3346b3d3109ab658c2601f6ece4442fe64f0c6b0
OTG-16 <oscar.torres5623@alumnos.udg.mx>
2022-03-17 17:12:33 -0600

    Modificación del formPersonas para que funcione debidamente el método old

--- resources/views/personas/formPersonas.blade.php
+++ resources/views/personas/formPersonas.blade.php
@@ -31,47 +31,47 @@
         @enderror
         <div class="mb-3">
             <label for='edad'>Edad (años):</label>
-            <input type="number" id="edad" name="edad" class="form-control" tabindex="2" value="{{ isset($persona) ? $persona->edad : '' }} {{ old('edad') }}">
+            <input type="number" id="edad" name="edad" class="form-control" tabindex="2" value="{{ old('edad') ?? $persona->edad ?? ''}}">
         </div>
         <div class="mb-3">
             <label for='peso'>Peso (kg):</label>
-            <input type="number" name="peso" step="0.1" class="form-control" tabindex="3" value="{{ isset($persona) ? $persona->peso : '' }} {{ old('peso') }}">
+            <input type="number" name="peso" step="0.1" class="form-control" tabindex="3" value="{{ old('peso') ?? $persona->peso ?? ''}}">
         </div>
         <div class="mb-3">
             <label for='estatura'>Estatura (m):</label>
-            <input type="number" name="estatura" step="0.1" class="form-control" tabindex="4" value="{{ isset($persona) ? $persona->estatura : '' }} {{ old('estatura') }}">
+            <input type="number" name="estatura" step="0.1" class="form-control" tabindex="4" value="{{ old('estatura') ?? $persona->estatura ?? ''}}">
         </div>
         <div class="mb-3">
             <label for='masa_muscular'>Masa muscular (%):</label>
-            <input type="number" name="masa_muscular" step="0.1" class="form-control" tabindex="5" value="{{ isset($persona) ? $persona->masa_muscular : '' }} {{ old('masa_muscular') }}">
+            <input type="number" name="masa_muscular" step="0.1" class="form-control" tabindex="5" value="{{ old('masa_muscular') ?? $persona->masa_muscular ?? ''}}">
         </div>
         <div class="mb-3">
             <label for='grasa_visceral'>Grasa visceral (%):</label>
-            <input type="number" name="grasa_visceral" step="0.1" class="form-control" tabindex="6" value="{{ isset($persona) ? $persona->grasa_visceral : '' }} {{ old('grasa_visceral') }}">
+            <input type="number" name="grasa_visceral" step="0.1" class="form-control" tabindex="6" value="{{ old('grasa_visceral') ?? $persona->grasa_visceral ?? ''}}">
         </div>
         <div class="mb-3">
         <label for='grasa_corporal'>Grasa corporal (%):</label>
-        <input type="number" name="grasa_corporal" step="0.1" class="form-control" tabindex="7" value="{{ isset($persona) ? $persona->grasa_corporal : '' }} {{ old('grasa_corporal') }}">
+        <input type="number" name="grasa_corporal" step="0.1" class="form-control" tabindex="7" value="{{ old('grasa_corporal') ?? $persona->grasa_corporal ?? ''}}">
         </div>
         <div class="mb-3">
             <label for='edad_metabolica'>Edad metabólica (años):</label>
-            <input type="number" id="edad_metabolica" name="edad_metabolica" class="form-control" tabindex="8" value="{{ isset($persona) ? $persona->edad_metabolica : '' }} {{ old('edad_metabolica') }}">
+            <input type="number" id="edad_metabolica" name="edad_metabolica" class="form-control" tabindex="8" value="{{ old('edad_metabolica') ?? $persona->edad_metabolica ?? ''}}">
         </div>
         <div class="mb-3">
             <label for='diferencia_de_la_edad'>Diferencia de la edad (años):</label>
-            <input type="number" id="diferencia_de_la_edad" name="diferencia_de_la_edad" tabindex="9" class="form-control" value="{{ isset($persona) ? $persona->diferencia_de_la_edad : '' }} {{ old('diferencia_de_la_edad') }}">
+            <input type="number" id="diferencia_de_la_edad" name="diferencia_de_la_edad" tabindex="9" class="form-control" value="{{ old('diferencia_de_la_edad') ?? $persona->diferencia_de_la_edad ?? ''}}">
         </div>
         <div class="mb-3">
             <label for='medida_de_brazo'>Medida de brazo (cm):</label>
-            <input type="number" name="medida_de_brazo" step="0.1" class="form-control" tabindex="10" value="{{ isset($persona) ? $persona->medida_de_brazo : '' }} {{ old('medida_de_brazo') }}">
+            <input type="number" name="medida_de_brazo" step="0.1" class="form-control" tabindex="10" value="{{ old('medida_de_brazo') ?? $persona->medida_de_brazo ?? ''}}">
         </div>
         <div class="mb-3">
             <label for='medida_de_pecho'>Medida de pecho (cm):</label>
-            <input type="number" name="medida_de_pecho" step="0.1" class="form-control" tabindex="11" value="{{ isset($persona) ? $persona->medida_de_pecho : '' }} {{ old('medida_de_pecho') }}">
+            <input type="number" name="medida_de_pecho" step="0.1" class="form-control" tabindex="11" value="{{ old('medida_de_pecho') ?? $persona->medida_de_pecho ?? ''}}">
         </div>
         <div class="mb-3">
             <label for='medida_de_pierna'>Medida de pierna (cm):</label>
-            <input type="number" name="medida_de_pierna" step="0.1" class="form-control" tabindex="12" value="{{ isset($persona) ? $persona->medida_de_pierna : '' }} {{ old('medida_de_pierna') }}">
+            <input type="number" name="medida_de_pierna" step="0.1" class="form-control" tabindex="12" value="{{ old('medida_de_pierna') ?? $persona->medida_de_pierna ?? ''}}">
         </div>
         <input type="submit" value="Guardar" class="btn btn-primary" tabindex="13" />
     </form>
